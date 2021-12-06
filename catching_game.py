a78e4e9c4463e40d664c23e233105b8608c8c7bd
21zhichua <91635596+21zhichua@users.noreply.github.com>
2021-12-06 14:02:23 -0800

    Update catching_game.py

--- catching_game.py
+++ catching_game.py
@@ -8,7 +8,7 @@ wn.addshape("basket.gif")
 basket = t.Turtle()
 basket.penup()
 basket.speed(0)
-basket.goto(0,-305)
+basket.goto(0,-288)
 basket.speed(6)
 basket.shape("basket.gif")
 #bacground color
@@ -17,7 +17,25 @@ wn.bgpic("forest.gif")
 counter = t.Turtle()
 counter.hideturtle()
 font_setup = ("Verdana", 50, "normal")
+#list making
+acorn_list = []
+for i in range(100):
+  acorn_list.append("acorn")
 
+
+acorn_cor_list = [-740, -640, -540, -440, -340, -240, -140, -40, 60, 160, 260, 360, 460, 560, 660, 730]
+acorn_speed = 2
+def draw_apple(index):
+  acorn_list[index].showturtle()
+  random_acorn_cor = r.randint(1,15)
+  acorn_list[index].penup()
+  acorn_list[index].shape("acorn.gif")
+  wn.tracer(False)
+  acorn_list[index].speed(acorn_speed)
+  acorn_list[index].setx(random_acorn_cor[random_acorn_cor])
+  acorn_list[index].goto(random_acorn_cor, -500)
+  wn.tracer(True)
+  wn.update()
 
 
 
@@ -43,16 +61,15 @@ update_score()
 #each movements
 def move_left():
   x = basket.xcor()
-  x -= 20
+  x -= 40
   basket.setx(x)
-  
+
 def move_right():
   x = basket.xcor()
   x += 40
   basket.setx(x)
 
 #keypress
-
 wn.onkeypress(move_left,"a")
 wn.onkeypress(move_right, "d")
 
